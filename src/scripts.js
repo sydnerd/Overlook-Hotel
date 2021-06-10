9502b273c768c0c6edf18d17733d32d5e7ca4108
Sydney Bear <sbear3915@gmail.com>
2021-06-10 10:50:53 -0600

    Add image2 to the main

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,6 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/img1.jpg'
+import './images/img2.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
