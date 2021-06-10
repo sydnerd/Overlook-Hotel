a2347a4ebe052b5abc5256ef924e83d060ed4625
Sydney Bear <sbear3915@gmail.com>
2021-06-10 17:42:39 -0600

    Complete get calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,22 +5,23 @@ const fetchAllData = () => {
     return Promise.all(promises)
         .catch(error => console.log("ERROR"))
 }
+
 const getCustomerData = () => {
-  fetch("http://localhost:3001/api/v1/customers")
+  return fetch("http://localhost:3001/api/v1/customers")
     .then(response => response.json())
     .catch(err => console.log("ERROR"))
 }
 
 
 const getRoomsData = () => {
-  fetch("http://localhost:3001/api/v1/rooms")
+  return fetch("http://localhost:3001/api/v1/rooms")
     .then(response => response.json())
     .catch(err => console.log("ERROR"))
 }
 
 
 const getBookingsData = () => {
-  fetch("http://localhost:3001/api/v1/bookings")
+  return fetch("http://localhost:3001/api/v1/bookings")
     .then(response => response.json())
     .catch(err => console.log("ERROR"))
 }
